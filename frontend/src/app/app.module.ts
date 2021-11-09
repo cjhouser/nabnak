86f4b07ee9e1674f52b71ee028aad49122cc2e47
Charles Houser <cjohouser@gmail.com>
2021-11-08 20:01:41 -0800

    regen angular proj

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from "@angular/material/toolbar";
 
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { BoardComponent } from './board/board.component';
-import { ColumnComponent } from './column/column.component';
 import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
-    BoardComponent,
-    ColumnComponent,
     CardComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    DragDropModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
